fbec9fd26419c341930f3232a0ff38c41ac42187
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-09-09 17:03:46 +0530

    Added CRUD apis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +16,30 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('items') // Define a new GET endpoint for fetching items
+  getItems(): string[] {
+    return this.appService.getItems();
+  }
+
+  @Get('items/:id') // Define a new GET endpoint for fetching a specific item by ID
+  getItemById(@Param('id') id: string): string {
+    return this.appService.getItemById(id);
+  }
+
+  @Post('items') // Define a new POST endpoint for creating an item
+  createItem(@Body() item: string): void {
+    this.appService.createItem(item);
+  }
+
+  @Put('items/:id') // Define a new PUT endpoint for updating an item by ID
+  updateItem(@Param('id') id: string, @Body() newItem: string): void {
+    this.appService.updateItem(id, newItem);
+  }
+
+  @Delete('items/:id') // Define a new DELETE endpoint for deleting an item by ID
+  deleteItem(@Param('id') id: string): void {
+    this.appService.deleteItem(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,38 @@ export class AppService {
   getHello(): string {
     return 'Hello EXE.LK';
   }
+
+  private readonly items: string[] = ['Item 1', 'Item 2', 'Item 3'];
+
+  getItems(): string[] {
+    return this.items;
+  }
+
+  getItemById(id: string): string {
+    const index = parseInt(id, 10);
+    if (isNaN(index) || index < 0 || index >= this.items.length) {
+      throw new Error('Item not found');
+    }
+    return this.items[index];
+  }
+
+  createItem(item: string): void {
+    this.items.push(item);
+  }
+
+  updateItem(id: string, newItem: string): void {
+    const index = parseInt(id, 10);
+    if (isNaN(index) || index < 0 || index >= this.items.length) {
+      throw new Error('Item not found');
+    }
+    this.items[index] = newItem;
+  }
+
+  deleteItem(id: string): void {
+    const index = parseInt(id, 10);
+    if (isNaN(index) || index < 0 || index >= this.items.length) {
+      throw new Error('Item not found');
+    }
+    this.items.splice(index, 1);
+  }
 }
